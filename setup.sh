ff3ff33fdda5a7dbc5025bd5878fa5aed860c712
Tap <visibletrap@gmail.com>
2015-12-02 08:41:22 +0800

    Add ffmpeg

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 apt-get update
 apt-get upgrade
-apt-get install -y ruby python youtube-dl
+apt-get install -y ruby python youtube-dl ffmpeg
 
 apt-get install -y tinyproxy
 sed -i -e 's/#Listen 192.168.0.1/Listen 127.0.0.1/g' /etc/tinyproxy.conf
